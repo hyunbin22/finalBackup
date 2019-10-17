f3dac4ba7f463015daaef2fe703114bb74d6f887
unknown <you@example.com>
2019-10-17 15:14:30 +0900

    부서,직급,사원 등록,수정,조회

--- finalProject/src/main/java/com/spring/bm/common/ArrayTypeHandler.java
+++ finalProject/src/main/java/com/spring/bm/common/ArrayTypeHandler.java
@@ -12,32 +12,33 @@ public class ArrayTypeHandler implements TypeHandler<String[]> {
 
 	@Override
 	public void setParameter(PreparedStatement ps, int i, String[] parameter, JdbcType jdbcType) throws SQLException {
-		// TODO Auto-generated method stub
-		//스트링 형식으로 값을 넣어주게 세팅
-		if(parameter!=null) {
-			ps.setString(i, String.join(",", parameter));
-		}
-		else {
+		// 배열을 스트링 형식으로 디비에 값을 넣어주게 세팅
+		if(parameter !=null) {
+			ps.setString(i, String.join(",", parameter));			
+		}else {
 			ps.setString(i, "");
 		}
 	}
 
+
 	@Override
 	public String[] getResult(ResultSet rs, String columnName) throws SQLException {
-		String temp=rs.getString(columnName);
+		// DB 에 있는 스트링 (, 로 나눠진 긴 스트링)을 배열로 바꿔주는것
+		String temp = rs.getString(columnName);
 		return temp.split(",");
 	}
 
 	@Override
 	public String[] getResult(ResultSet rs, int columnIndex) throws SQLException {
-		String temp=rs.getString(columnIndex);
+		// DB 에 있는 스트링 ( "," 로 나눠진 긴 스트링)을 배열 (인덱스 번호)로 바꿔 원하는 값을 가지고 오는 메소드
+		String temp = rs.getString(columnIndex);
 		return temp.split(",");
 	}
 
 	@Override
 	public String[] getResult(CallableStatement cs, int columnIndex) throws SQLException {
-		String temp=cs.getString(columnIndex);
-		return temp.split(",");
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
